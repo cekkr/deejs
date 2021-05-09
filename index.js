94a8c266384ef1b8cc2a05894602d6d7f0625304
Riccardo Cecchini <rcecchini.ds@gmail.com>
2021-05-09 10:35:20 +0200

    Bom qualcosa ho fatto

--- index.js
+++ index.js
@@ -1,20 +1,67 @@
 const https = require('https');
 const fs = require('fs');
+let ejs = require('ejs');
 
 const options = {
   key: fs.readFileSync('privateKey.key'),
   cert: fs.readFileSync('certificate.crt')
 };
 
+var supportedFormats = ['ejs','html']
+
+function searchPath(url){
+    url = 'htdocs/'+url;
+
+    if(fs.existsSync(url)){
+        if(fs.lstatSync(url).isDirectory()){
+            for(var format of supportedFormats){
+                if(fs.existsSync(url + "."+format)){
+                    url += "."+format;
+                    break;
+                }
+            }
+        }
+    } else {
+        var split = url.split('/');
+        var cur = '';
+        var lastBlackHole = '';
+        var req = '';
+        for(var s of split){
+            cur += s +'/';
+            if(!fs.existsSync(cur)){
+                req += '/'+s;
+            }
+)
+            if(fs.existsSync(cur+'_blackhole.ejs')){
+                lastBlackHole = cur+'_blackhole.ejs';
+                req = '';
+            }
+        }
+
+        return {exists: fs.existsSync(lastBlackHole), file: lastBlackHole, req: req};
+    }
+
+    return {exists: fs.existsSync(url), file: url};
+}
+
 https.createServer(options, (req, res) => {
-    console.log(req.url);
-    res.writeHead(200);
-    res.end('hello world\n');
+    //Analyze req.url
+    var analyze = searchPath(req.url);
+
+    console.log(analyze);
+
+    if(!analyze.exists){
+        res.writeHead(404);
+        res.end('Page not found');
+    }
+    else {
+        res.writeHead(200);
+        res.end('hello world\n');
+    }
 
 }).listen(8000);
 
-let ejs = require('ejs');
+
 let people = function(){ return "test"; };
 let html = ejs.render('<%= people(); %>', {people: people});
-
 console.log(html);
